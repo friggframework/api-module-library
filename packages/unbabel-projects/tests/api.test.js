6463c8f422c5a6dbaea7def1339335bb0171fa37
michael webber <michaelryanwebber@gmail.com>
2024-07-11 19:20:57 -0400

    add a test to upload timing regenerate lock file after removing node_modules

--- packages/unbabel-projects/tests/api.test.js
+++ packages/unbabel-projects/tests/api.test.js
@@ -98,4 +98,50 @@ describe('Unbabel Projects API Tests', () => {
             expect(response.status).toBe(200);
         });
     })
+
+    describe.skip('Large File Test', () => {
+        let projectId;
+        it('Should create the project', async () => {
+            const projectDef = {
+                "name": `large_file_test_project_${Date.now()}`,
+                "pipeline_ids": ["3733936f-5a31-465d-9722-ae476659f3b7"],
+                "requested_by": "michael.webber@lefthook.com"
+            }
+            const response = await api.createProject(projectDef,  'https://webhook.site/3812d00d-ff11-4a91-a931-3ecb813fc90e');
+            expect(response).toBeDefined();
+            expect(response.status).toBe('created');
+            projectId = response.id;
+        });
+        it('Should retrieve the project', async () => {
+            const response = await api.getProject(projectId);
+            expect(response).toBeDefined();
+            expect(response.id).toBe(projectId);
+        });
+        let fileId;
+        it('Should add a file to the project', async () => {
+            const response = await api.addFileToProject(projectId, 'test.txt', 'test file','txt');
+            expect(response).toBeDefined();
+            expect(response.upload_url).toBeDefined();
+            fileId = response.id;
+        });
+        it('Should upload file to the upload url', async () => {
+            const response = await api.getFile(projectId, fileId);
+            expect(response).toBeDefined();
+            expect(response.upload_url).toBeDefined();
+            const file = fs.readFileSync('tests/test.txt', 'utf8');
+            const response2 = await api.uploadFile(response.upload_url, file.repeat(10000));
+            const response3 = await api.submitProject(projectId);
+            expect(response2).toBeDefined();
+            expect(response2.status).toBe(200);
+            expect(response3).toBeDefined();
+            expect(response3.status).toBe(
+                'submitted'
+            );
+        });
+        it('Should delete the project', async () => {
+            const response = await api.cancelProject(projectId);
+            expect(response).toBeDefined();
+            expect(response.status).toBe(200);
+        });
+    })
 });

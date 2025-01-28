1d1e56a875eefa22c49fb34d02db96e60a18d760
Sean Matthews <sean.matthews@lefthook.co>
2025-01-28 12:03:51 -0500

    Getting v1/v2 ready

--- packages/v1-ready/frontify/definition.js
+++ packages/v1-ready/frontify/definition.js
@@ -9,41 +9,69 @@ const Definition = {
         return config.name
     },
     moduleName: config.name,
-    modelName: 'Asana',
+    modelName: 'Frontify',
     requiredAuthMethods: {
+        getAuthorizationRequirements: async function (api) {
+            return {
+                url: api.getAuthUri(),
+                type: 'oauth2',
+                data: {
+                    jsonSchema: {
+                        title: 'Auth Form',
+                        type: 'object',
+                        required: ['domain'],
+                        properties: {
+                            domain: {
+                                type: 'string',
+                                title: 'Your Frontify Domain',
+                            }
+                        }
+                    },
+                    uiSchema: {
+                        domain: {
+                            'ui:help':
+                                'A Frontify domain, e.g: lefthook.frontify.com',
+                            'ui:placeholder': 'Your Frontify domain...',
+                        },
+                    }
+                }
+            };
+        },
         getToken: async function (api, params) {
             const code = get(params.data, 'code');
+            const domain = get(params.data, 'domain');
+            api.setDomain(domain);
             return api.getTokenFromCode(code);
         },
         getEntityDetails: async function (api, callbackParams, tokenResponse, userId) {
-            const userDetails = await api.getUserDetails();
+            const {user: userDetails} = await api.getUser();
             return {
-                identifiers: {externalId: userDetails.sub, user: userId},
-                details: {name: userDetails.name, email: userDetails.email},
+                identifiers: {externalId: userDetails.id, user: userId},
+                details: {name: userDetails.name },
             }
         },
         apiPropertiesToPersist: {
             credential: [
-                'access_token', 'refresh_token'
+                'access_token', 'refresh_token', 'domain'
             ],
             entity: [],
         },
         getCredentialDetails: async function (api, userId) {
-            const userDetails = await api.getUserDetails();
+            const {user: userDetails} = await api.getUser();
             return {
-                identifiers: {externalId: userDetails.portalId, user: userId},
+                identifiers: {externalId: userDetails.id, user: userId},
                 details: {}
             };
         },
         testAuthRequest: async function (api) {
-            return api.getUserDetails()
+            return api.getUser()
         },
     },
     env: {
-        client_id: process.env.ASANA_CLIENT_ID,
-        client_secret: process.env.ASANA_CLIENT_SECRET,
-        scope: process.env.ASANA_SCOPE,
-        redirect_uri: `${process.env.REDIRECT_URI}/asana`,
+        client_id: process.env.FRONTIFY_CLIENT_ID,
+        client_secret: process.env.FRONTIFY_CLIENT_SECRET,
+        scope: process.env.FRONTIFY_SCOPE,
+        redirect_uri: `${process.env.REDIRECT_URI}/frontify`,
     }
 };
 

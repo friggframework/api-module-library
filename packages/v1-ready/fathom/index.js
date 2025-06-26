b7c876b75fe0d099872f9522ccfb4249aa5f49d8
Sean Matthews <sean.matthews@lefthook.co>
2025-06-25 21:02:04 -0400

    Just let claude code do its thing. So far, _looks_ good, TBD on behavior.

---
+++ packages/v1-ready/fathom/index.js
@@ -0,0 +1,9 @@
+const { Api } = require('./api');
+const { Definition } = require('./definition');
+const config = require('./defaultConfig.json');
+
+module.exports = {
+    Api,
+    Definition,
+    config,
+};
---
+++ packages/v1-ready/fathom/jest-teardown.js
@@ -0,0 +1,3 @@
+module.exports = async () => {
+    // Add any global teardown logic here if needed
+};
